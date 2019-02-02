c131579de60ada29a345918e002e27d81ad6b9ec
Erislandio Soares <erislandiosoares21@gmail.com>
2019-02-02 12:52:16 -0200

    Adição do schema Post mongo db

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+
+// database setup
+
+mongoose.connect('mongodb://localhost:27017/upload', {
+    useNewUrlParser: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
